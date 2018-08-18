71bc5a2d8bec3c607c63cae6f0ff4a528e8af20d
Gagan171991 <gaganveerSood@gmail.com>
2018-08-18 20:37:43 +0530

    Add files via upload

---
+++ v11.8/When-In-India.bat
@@ -0,0 +1,6 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+64.exe -epool daggerhashimoto.in.nicehash.com:3353 -ewal 3Aj4kHu2umufERNpXmTwvwd9ae4jWoDvXB.Nish -epsw x -esm 3 -allpools 1 -estale 0 -mode 1 -dbg -1 -mport 0
